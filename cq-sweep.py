cfb7a613e633c3384b7cffe7db7bb701b8a447cf
Wink Saville <wink@saville.com>
2025-05-05 15:35:34 -0700

    feat: Take parameters for pts and tangents for creating a spline
    The parameters maybe in any order but pts is required

--- cq-sweep.py
+++ cq-sweep.py
@@ -2,10 +2,33 @@
 # Initially based on https://github.com/CadQuery/cadquery/blob/master/examples/Ex023_Sweep.py
 import cadquery as cq
 from cadquery.vis import show
+import sys
 
-#pts = [(0, 0, 0), (0, 0, 1)] # path 2 points on Z axis
-pts = [(0, 0, 0), (1, 1, 1)] # path 2 points 45 degrees diaginal to (1,1,1)
-path = cq.Workplane("XY").spline(pts)
+if len(sys.argv) <= 1 or len(sys.argv) > 3:
+    print("Usage: cq-sweep.py pts=<list> tangents=<list>")
+    print('Example: cq-sweep.py pts="[(0,0,0),(10,0,10)]" tangents="[(0,0,1),(10,0,10)]"')
+    print(" Note: Order doesn't matter and tangents are optional.")
+
+    sys.exit(1)
+
+# Parse the arguments
+for arg in sys.argv[1:]:
+    if "=" not in arg:
+        print(f"Invalid argument: {arg}")
+        sys.exit(1)
+    key, value = arg.split("=")
+    if key == "pts":
+        pts = eval(value)
+    elif key == "tangents":
+        tangents = eval(value)
+    else:
+        print(f"Unknown argument: {key}")
+        sys.exit(1)
+
+print(f"pts: {pts}")
+print(f"tangents: {tangents}")
+
+path = cq.Workplane("XY").spline(pts, tangents)
 
 # Create a cylinder with a radius of 0.5 along the path
 cylinder = cq.Workplane("XY").circle(0.5).sweep(path)
