371fd74f4493999a000fa8d901824b4f0e3245a4
Wink Saville <wink@saville.com>
2025-05-09 19:08:28 -0700

    feat: Tapered support
    User can specify radius of top and bottom as well as the location
    of the mid section

--- cq-sweep.py
+++ cq-sweep.py
@@ -6,7 +6,29 @@ import sys
 import argparse
 import math
 
+default_mid_location = 0.5
+default_base_radius = 0.5
+default_top_radius = 0.15
+
 argparse = argparse.ArgumentParser(description="Sweep a cylinder along a path defined by points and optional tangents.")
+argparse.add_argument(
+    "-br","--base_radius",
+    type=float,
+    default=default_base_radius,
+    help=f"Base radius of the cylinder. Example: --base_radius={default_base_radius}",
+)
+argparse.add_argument(
+    "-tr","--top_radius",
+    type=float,
+    default=default_top_radius,
+    help=f"Top radius of the cylinder. Example: --top_radius={default_top_radius}",
+)
+argparse.add_argument(
+    "-ml","--mid_location",
+    type=float,
+    default=default_mid_location,
+    help=f"Location of mid section where transition begins to top_radius. value between 0..1. Example: --mid_location={default_mid_location}",
+)
 argparse.add_argument(
     "-p","--pts",
     type=str,
@@ -33,8 +55,22 @@ else:
 
 path = cq.Workplane("XY").spline(pts, tangents)
 
-# Create a cylinder with a radius of 0.5 along the path
-cylinder = cq.Workplane("XY").circle(0.5).sweep(path)
+bottom_loc = [path.val().locationAt(0)]
+middle_loc = [path.val().locationAt(args.mid_location)]
+top_loc = [path.val().locationAt(1)]
+print(f"Bottom location: {bottom_loc[0].toTuple()}")
+print(f"middle location: {middle_loc[0].toTuple()}")
+print(f"top location: {top_loc[0].toTuple()}")
+
+base_radius = 0.5
+middle_radius = 0.5
+top_radius = 0.15
+
+wp = cq.Workplane("XY").pushPoints(bottom_loc).circle(base_radius)
+wp = wp.pushPoints(middle_loc).circle(middle_radius)
+wp = wp.pushPoints(top_loc).circle(top_radius)
+#wp = wp.consolidateWires()
+shape = wp.sweep(path, multisection=True)
 
 # Define the normal vectors for the XY, XZ, and YZ planes
 xy_plane_normal = cq.Vector(0, 0, 1)
@@ -46,7 +82,7 @@ def angle_of_plane(normal, plane_normal):
     return math.degrees(normal.getAngle(plane_normal))
 
 # Get the normal of the top face of the cylinder
-top_face = cylinder.faces(">Z").first().val()
+top_face = shape.faces(">Z").first().val()
 normal_top_face = top_face.normalAt(top_face.Center())
 
 top_face_angles = {
@@ -57,7 +93,7 @@ top_face_angles = {
 print(f"Top face angles: {top_face_angles}")
 
 # Get the normal of the bottom face of the cylinder
-bottom_face = cylinder.faces("<Z").first().val()
+bottom_face = shape.faces("<Z").first().val()
 normal_bottom_face = bottom_face.normalAt(bottom_face.Center())
 
 bottom_face_angles = {
@@ -67,4 +103,4 @@ bottom_face_angles = {
 }
 print(f"Bottom face angles: {bottom_face_angles}")
 
-show(cylinder, title=f"pts: {pts}, tangents: {tangents}, angles XY:{top_face_angles["XY"]:.2f}, XZ:{top_face_angles["XZ"]:.2f}, YZ:{top_face_angles["YZ"]:.2f}")
+show(shape, title=f"pts: {pts}, tangents: {tangents}, angles XY:{top_face_angles["XY"]:.2f}, XZ:{top_face_angles["XZ"]:.2f}, YZ:{top_face_angles["YZ"]:.2f}")
